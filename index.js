5ac2117bc2de1720ad4e7fe328fe07baa297a31d
joshua-barnard-evolv <78709720+joshua-barnard-evolv@users.noreply.github.com>
2021-03-09 06:27:09 +1000

    added mutation observer

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 
 var watchForChanges = function () {
-    console.log('test');
+    console.log('Integration start!');
+
+    // Select the node that will be observed for mutations
+    const targetNode = document.querySelectorAll('.wrapper')[0]
+
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true };
+
+    // Callback function to execute when mutations are observed
+    const callback = function () {
+        evolv?.rerun();
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+
+    // Later, you can stop observing
+    // observer.disconnect();
 }
 
-module.exports = function(config){
+module.exports = function (config) {
     watchForChanges();
 }
